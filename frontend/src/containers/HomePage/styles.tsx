0e51e1cf1c6dfb12857165c2f05b5d0d050b6738
PedroPassos87 <peagacarreira@gmail.com>
2025-07-20 18:02:32 -0300

    feat: grid

--- frontend/src/containers/HomePage/styles.tsx
+++ frontend/src/containers/HomePage/styles.tsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
 
-export const Container = styled.section`
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr ));
+  grid-gap: 3rem;
+
+
 `;
