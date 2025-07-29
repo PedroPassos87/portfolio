b355e2cbf68512770ef2375842dcac627cda1883
PedroPassos87 <peagacarreira@gmail.com>
2025-07-29 16:29:21 -0300

    recomeço

--- frontend/src/config/app-config.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = process.env.NEXT_PUBLIC_BASE_URL;
-export const ALL_POSTS_URL = `${API_URL}/api/articles`;
-
-export const SITE_NAME = 'Mithril Core';
-export const SITE_URL = '';
--- frontend/src/data/articles/get-all-articles.ts
+++
@@ -1,9 +0,0 @@
-import { ALL_POSTS_URL } from "@/config/app-config";
-import { ArticleData } from "@/domain/articles";
-import { fetchJson } from "@/utils/fetch-json";
-
-export const getAllArticles = async (): Promise<ArticleData[]> => {
-  const url = `${ALL_POSTS_URL}?populate=*`;
-  const articles = await fetchJson<ArticleData[]>(url);
-  return articles;
-}
